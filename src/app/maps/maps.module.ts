511bc76219d18fcc75702124ba2cb81a5520afd3
Germán <german.palencia@adentia.es>
2023-09-27 13:16:49 +0200

    Element Ref Map

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiZ3VtaTU0MyIsImEiOiJjbG4wOWV0MHIwbTVjMnVtdm5xb3E4dHZoIn0.uiJoIOMx3ZcEigO-iXuWSg';
+
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,7 +1,6 @@
-import { AfterViewInit, Component } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-(mapboxgl as any).accessToken = 'pk.eyJ1IjoiZ3VtaTU0MyIsImEiOiJjbG4wOWV0MHIwbTVjMnVtdm5xb3E4dHZoIn0.uiJoIOMx3ZcEigO-iXuWSg';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
@@ -10,13 +9,18 @@ import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-g
 
 export class FullScreenPageComponent implements AfterViewInit{
 
+  @ViewChild('map') divMap?: ElementRef;
+
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+
+    if(!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
-      });
+    });
   }
 
 
